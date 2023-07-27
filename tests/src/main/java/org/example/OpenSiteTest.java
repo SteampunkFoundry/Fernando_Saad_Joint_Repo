01024475060352334ed6e01c23fb93f0060ae787
Saad N <niaziksaad@gmail.com>
2023-07-27 18:31:24 -0400

    interface

--- tests/src/main/java/org/example/OpenSiteTest.java
+++ tests/src/main/java/org/example/OpenSiteTest.java
@@ -11,28 +11,11 @@ import static org.testng.Assert.assertEquals;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class OpenSiteTest {
+public class OpenSiteTest extends Main{
     @Test
     public void functionalTest() {
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
 
-        //**used to get path to index.html, which will be used by the driver**
-        //get current path
-        String currentPath = System.getProperty("user.dir");
-
-        //file leading to current path
-        File tempFile = new File(currentPath);
-
-        //get parent file of tempfile
-        File projectPathFile = tempFile.getParentFile();
-        //get parent pathway (same as project)
-        String projectPath = projectPathFile.getAbsolutePath();
-        //concatenate with /index.html
-        String indexPath = (projectPath + "/index.html");
-
-        //get index.html
-        driver.get(indexPath);
+        WebDriver driver = loadSite();
 
         //verify the title appears correctly, which will happen if
         //it opens
---
+++ tests/src/main/java/org/example/TestInterface.java
@@ -0,0 +1,7 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+
+public interface TestInterface {
+    public WebDriver loadSite();
+}

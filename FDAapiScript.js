fc67f60c85df785492682f166d3136d3a446ae50
SteampunkFernando <fernando.quezado@steampunk.com>
2023-07-28 18:52:02 -0400

    Updated javascript file and added reason for recall and added some styling on the website!

--- FDAapiScript.js
+++ FDAapiScript.js
@@ -5,14 +5,14 @@ async function getReports() {
     // Define a regex for 5 digit ZIP code
     let zipRegex = /^[0-9]{5}$/;
 
-    // Check if the input matches the 5 digit ZIP code format
+    // Check if the input matches the 5-digit ZIP code format
     if (!zipRegex.test(zipCode)) {
         alert("Please enter a valid 5 digit ZIP code.");
         return;
     }
 
     // Define your API endpoint with the zip code as a query parameter
-    let url = `https://api.fda.gov/food/enforcement.json?search=postal_code:${zipCode}&limit=5`;
+    let url = `https://api.fda.gov/food/enforcement.json?search=${zipCode}&limit=5`;
 
     // Fetch the data from the API
     let response = await fetch(url);
@@ -20,18 +20,26 @@ async function getReports() {
     // Check the response status
     if (!response.ok) {
         // If the response status is not ok, throw an error
+        alert("Nothing is recalled at that ZIP code!");
         throw new Error(`HTTP error! status: ${response.status}`);
     } else {
         // If the response status is ok, parse the JSON data
         let data = await response.json();
 
+
         // Clear previous results
         document.getElementById("result").innerHTML = "";
 
         // Display the data in the "result" div
+// Display the data in the "result" div
         data.results.forEach((item, i) => {
             document.getElementById("result").innerHTML +=
-                `<p>Report ${i+1}: ${item.product_description}</p>`;
+                `<tr>
+            <td>${i+1}</td>
+            <td>${item.product_description}</td>
+            <td>${item.reason_for_recall}</td>
+        </tr>`;
         });
+
     }
 }

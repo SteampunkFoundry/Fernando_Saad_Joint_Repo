10f2605717da0cc2edef18c2d4d24149afb30c39
Saad N <niaziksaad@gmail.com>
2023-07-31 17:36:19 -0400

    screenshots

--- tests/src/main/java/org/example/IncorrectZipTest.java
+++ tests/src/main/java/org/example/IncorrectZipTest.java
@@ -1,23 +1,14 @@
 package org.example;
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
-import org.testng.asserts.Assertion;
-
-import java.util.concurrent.TimeUnit;
 
 import static org.testng.Assert.assertEquals;
 
 
-public class IncorrectZipTest extends Main {
-    WebDriver driver;
+public class IncorrectZipTest extends Main{
     String expectedErrorMessage = ("Please enter a valid 5 digit ZIP code.");
 
-    //reduce redundancy and repeating code
-    //NEED TO load call method "loadSite()" before you call this
     public void inputIntoZipBox(String input){
         //find the zipcode input box and input the parameter
         WebElement zipCodeBox = driver.findElement(By.id("zipCode"));
@@ -36,36 +27,28 @@ public class IncorrectZipTest extends Main {
     //check using zipcode larger than 5 digits
     @Test
     public void zipGreaterThanFive() {
-        driver = loadSite();
         inputIntoZipBox("123456");
         checkAlert();
-        driver.quit();
     }
 
     //check using zipcode with less than 5 digits
     @Test
     public void zipLessThanFive(){
-        driver = loadSite();
         inputIntoZipBox("1234");
         checkAlert();
-        driver.quit();
     }
 
     //check using letters
     @Test
     public void zipRandomLetters(){
-        driver = loadSite();
         inputIntoZipBox("abcde");
         checkAlert();
-        driver.quit();
     }
 
     //check using random symbols
     @Test
     public void zipRandomSymbols(){
-        driver = loadSite();
         inputIntoZipBox("!@#$%");
         checkAlert();
-        driver.quit();
     }
 }
--- tests/src/main/java/org/example/OpenSiteTest.java
+++ tests/src/main/java/org/example/OpenSiteTest.java
@@ -1,25 +1,12 @@
 package org.example;
 
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
-
-import java.io.File;
-
 import static org.testng.Assert.assertEquals;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class OpenSiteTest extends Main{
+public class OpenSiteTest extends Main {
     @Test
     public void functionalTest() {
-
-        WebDriver driver = loadSite();
-
-        //verify the title appears correctly, which will happen if
-        //it opens
+        //verify the title appears correctly, which will happen if it opens
         assertEquals(driver.getTitle(), "My Project for Team Punk");
-
-        //Close the browser
-        driver.quit();
     }
 }

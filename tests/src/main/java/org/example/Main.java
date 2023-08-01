44f0178d8c06789e70f987529ac6500ab9f31dc3
SteampunkFernando <fernando.quezado@steampunk.com>
2023-08-01 10:03:31 -0400

    added files from testing branch

--- tests/src/main/java/org/example/Main.java
+++ tests/src/main/java/org/example/Main.java
@@ -1,15 +1,30 @@
 package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 
 import java.io.File;
+import java.io.IOException;
 
-public class Main implements TestInterface {
-    public WebDriver loadSite() {
+public class Main {
+
+    WebDriver driver;
+    String projectPath;
+    //before method which loads up site (headless)
+    @BeforeMethod
+    public void loadSite() {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.setHeadless(true);
+        driver = new ChromeDriver(options);
 
         //**used to get path to index.html, which will be used by the driver**
         //get current path
@@ -21,10 +36,18 @@ public class Main implements TestInterface {
         //get parent file of tempfile
         File projectPathFile = tempFile.getParentFile();
         //get parent pathway (same as project)
-        String projectPath = projectPathFile.getAbsolutePath();
+        projectPath = projectPathFile.getAbsolutePath();
         //concatenate with /index.html
         String indexPath = (projectPath + "/index.html");
         driver.get(indexPath);
-        return driver;
+    }
+
+    //after method that takes screenshot, saves screenshot into screenshots folder, and closes driver
+    @AfterMethod
+    public void tearDown(ITestResult result) throws IOException {
+        TakesScreenshot screenshot = (TakesScreenshot)driver;
+        File source = screenshot.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(source, new File(projectPath+ "/screenshots/" + result.getMethod().getMethodName() + ".png"));
+        driver.quit();
     }
 }
--- tests/src/main/java/org/example/TestInterface.java
+++
@@ -1,7 +0,0 @@
-package org.example;
-
-import org.openqa.selenium.WebDriver;
-
-public interface TestInterface {
-    public WebDriver loadSite();
-}
